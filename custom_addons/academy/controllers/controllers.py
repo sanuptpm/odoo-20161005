279ad8379b4dfd7dac1a387ea147c8bac1d105a9
sanu <sanuptpm@gmail.com>
2017-03-21 17:29:52 +0530

    academy first commit for website

--- custom_addons/academy/controllers/controllers.py
+++ custom_addons/academy/controllers/controllers.py
@@ -1,9 +1,17 @@
 # -*- coding: utf-8 -*-
 from odoo import http
 
+# class Academy(http.Controller):
+#     @http.route('/academy/academy/', auth='public')
+#     def index(self, **kw):
+#         return http.request.render('academy.index', {
+#             'teachers': ["Diana Padilla", "Jody Caroll", "Lester Vaughn"],
+#         })
+
 class Academy(http.Controller):
-    @http.route('/academy/academy/', auth='public')
+    @http.route('/academy/academy/', auth='public', website=True)
     def index(self, **kw):
         return http.request.render('academy.index', {
             'teachers': ["Diana Padilla", "Jody Caroll", "Lester Vaughn"],
         })
+
--- custom_addons/academy/models/models.py
+++ custom_addons/academy/models/models.py
@@ -13,3 +13,8 @@ from odoo import models, fields, api
 #     @api.depends('value')
 #     def _value_pc(self):
 #         self.value2 = float(self.value) / 100
+
+class Teachers(models.Model):
+    _name = 'academy.teachers'
+
+    name = fields.Char()
