e3acdf123bab854992aee9bc4d1d72b730a91bbc
sanu <sanuptpm@gmail.com>
2017-03-21 16:29:19 +0530

    academy first template

--- custom_addons/academy/controllers/controllers.py
+++ custom_addons/academy/controllers/controllers.py
@@ -4,5 +4,6 @@ from odoo import http
 class Academy(http.Controller):
     @http.route('/academy/academy/', auth='public')
     def index(self, **kw):
-        return "Hello, world"
-
+        return http.request.render('academy.index', {
+            'teachers': ["Diana Padilla", "Jody Caroll", "Lester Vaughn"],
+        })
