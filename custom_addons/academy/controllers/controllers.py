28f9c9af4a306d8a2f7a90fdb583a094084197cb
sanu <sanuptpm@gmail.com>
2017-03-22 12:56:37 +0530

    update

--- custom_addons/academy/controllers/controllers.py
+++ custom_addons/academy/controllers/controllers.py
@@ -11,7 +11,8 @@ from odoo import http
 class Academy(http.Controller):
     @http.route('/academy/academy/', auth='public', website=True)
     def index(self, **kw):
+        Teachers = http.request.env['academy.teachers']
         return http.request.render('academy.index', {
-            'teachers': ["Diana Padilla", "Jody Caroll", "Lester Vaughn"],
+            'teachers': Teachers.search([])
         })
 
