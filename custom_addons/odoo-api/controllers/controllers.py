f251202528e0299709404a4f2b0ef95f421865c1
sanu <sanu.mn@pitsolutions.com>
2017-04-06 15:37:08 +0530

    odoo-api commit for fixed bugs in sign_up

--- custom_addons/odoo-api/controllers/controllers.py
+++ custom_addons/odoo-api/controllers/controllers.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from odoo import http
-
+from odoo.exceptions import ValidationError
 from odoo.http import request
 import xmlrpclib
 
@@ -22,15 +22,26 @@ class MyController(http.Controller):
             msg = "LOGIN_FAIL"
         return {"status": 0, "message": msg, "data": []}
 
+    # http://localhost:8000/sign_up   {"params": {"username":"demo","password":"demo","new_name":"name","new_login":"new3","new_password":"password"}}
     @http.route('/sign_up', type='json', methods=['POST'], auth='user', csrf=False)
     def sign_up(self, **args):
+        myurs = request.env['res.users'].search([])
         url = "http://localhost:8000"
         db = "apidb"
         username = args.get('username') # username
         password = args.get('password') # password
 
+        new_name = args.get('new_name')
+        new_login = args.get('new_login')
+        new_password = args.get('new_password')
+
         common = xmlrpclib.ServerProxy('{}/xmlrpc/2/common'.format(url))
         uid = common.authenticate(db, username, password, {}) # check user in model
         models = xmlrpclib.ServerProxy('{}/xmlrpc/2/object'.format(url))
-
-        new_user_id = models.execute_kw(db, uid, password, 'res.users', 'create', [{'name':"newuser", 'login':'newuser', 'new_password':'password'}])
+        
+        for x in myurs:
+            if x.login == new_login:
+                raise ValidationError("User already exists!")
+        new_user_id = models.execute_kw(db, uid, password, 'res.users', 'create', [{'name':new_name, 'login':new_login, 'new_password':new_password}])
+        
+        return {"status": 0, "message": new_user_id, "data": []}
