5992467ffa1d4c0682b1a730fe1b4495e8fe95ea
sanu <sanu.mn@pitsolutions.com>
2017-04-06 12:52:17 +0530

    odoo-api commit for new file test and remove unwanted code

--- custom_addons/odoo-api/controllers/controllers.py
+++ custom_addons/odoo-api/controllers/controllers.py
@@ -5,13 +5,7 @@ from odoo.http import request
 import xmlrpclib
 
 class MyController(http.Controller):
-    @http.route('/hello/world/', type="http", auth='public')
-    # @http.route('/hello/world/', auth='user')
-    def index(self, **kw):
-        myobj = request.env['res.partner'].search([])
-        for x in myobj:
-            print "======res.partner==name=====", x.name
-        return "ok"
+
     # http://localhost:8000/login  {"params": {"username":"admin","password":"admin"}}
     @http.route('/login', type='json', methods=['POST'], auth='user', csrf=False)
     def login(self, **args):
@@ -22,10 +16,11 @@ class MyController(http.Controller):
 
         common = xmlrpclib.ServerProxy('{}/xmlrpc/2/common'.format(url))
         uid = common.authenticate(db, username, password, {})
-
         if uid == 1 :
             msg = "SUCCESS_LOGIN"
         else:
             msg = "LOGIN_FAIL"
-
         return {"status": 0, "message": msg, "data": []}
+
+    def sign_up():
+        pass
