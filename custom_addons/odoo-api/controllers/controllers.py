144644e722b39288aaaeee50d16a3ad27757541d
sanu <sanu.mn@pitsolutions.com>
2017-04-06 12:34:16 +0530

    odoo-api commit for res.users login

--- custom_addons/odoo-api/controllers/controllers.py
+++ custom_addons/odoo-api/controllers/controllers.py
@@ -12,4 +12,20 @@ class MyController(http.Controller):
         for x in myobj:
             print "======res.partner==name=====", x.name
         return "ok"
-    
+    # http://localhost:8000/login  {"params": {"username":"admin","password":"admin"}}
+    @http.route('/login', type='json', methods=['POST'], auth='user', csrf=False)
+    def login(self, **args):
+        url = "http://localhost:8000"
+        db = "apidb"
+        username = args.get('username') # username
+        password = args.get('password') # password
+
+        common = xmlrpclib.ServerProxy('{}/xmlrpc/2/common'.format(url))
+        uid = common.authenticate(db, username, password, {})
+
+        if uid == 1 :
+            msg = "SUCCESS_LOGIN"
+        else:
+            msg = "LOGIN_FAIL"
+
+        return {"status": 0, "message": msg, "data": []}
